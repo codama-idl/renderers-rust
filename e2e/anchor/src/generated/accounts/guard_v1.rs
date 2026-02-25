90a6dd80c97c87805f78a94c0bb1ddf8193983c8
yihau <yihau.chen@icloud.com>
2026-02-25 20:03:49 +0800

    remove trailing comma

--- e2e/anchor/src/generated/accounts/guard_v1.rs
+++ e2e/anchor/src/generated/accounts/guard_v1.rs
@@ -72,7 +72,7 @@ pub fn fetch_all_guard_v1(
     for i in 0..addresses.len() {
         let address = addresses[i];
         let account = accounts[i].as_ref().ok_or(std::io::Error::other(format!(
-            "Account not found: {address}",
+            "Account not found: {address}"
         )))?;
         let data = GuardV1::from_bytes(&account.data)?;
         decoded_accounts.push(crate::shared::DecodedAccount {
